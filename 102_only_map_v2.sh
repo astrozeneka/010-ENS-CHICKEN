e62b301efaef2c7407e3256904cf598f8d3bf032
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-10-31 09:41:45 +0700

    Add 102 for only filtering the mapped reads

---
+++ 102_only_map_v2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -n 1 -c 64
+#SBATCH -t 24:00:00
+#SBATCH -J OnlyMap
+#SBATCH -A proj5034
+
+module purge
+module load SAMtools/1.9-intel-2019b
+
+genomes=(
+  "SRR17968711"
+  "SRR21763962"
+  "SRR24263490"
+  "SRR24608364"
+  "SRR24608384"
+  "SRR24608387"
+  "SRR24608388"
+  "SRR24608392"
+  "SRR24608448"
+  "SRR25731328"
+  "SRR25998541"
+)
+mkdir -p only_map_v2
+for genome in "${genomes[@]}"
+do
+  echo "${genome}"
+  samtools view -@ 64 -h F 4 "data/raw_sam_v2/${genome}.sam" > "data/only_map_v2/${genome}.sam"
+done
